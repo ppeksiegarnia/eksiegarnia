d158accfee0b355498d9dfd281a1b044305780f9
Johny <plusfourgamer@gmail.com>
2018-04-17 13:54:24 +0200

    Few modifications

---
+++ src/main/java/pl/controller/CheckoutController.java
@@ -0,0 +1,19 @@
+package pl.controller;
+
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CheckoutController {
+
+    @GetMapping("/checkout")
+    public String home(Model model)
+    {
+        new CheckAuth(model);
+        return "checkout";
+    }
+}
