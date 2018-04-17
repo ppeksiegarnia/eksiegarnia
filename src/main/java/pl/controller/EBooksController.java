d3d3d7ad33fa07f67d3ed1830fcc357b3f044ee0
Johny <plusfourgamer@gmail.com>
2018-04-17 14:52:55 +0200

    Fix for books, new controllers

---
+++ src/main/java/pl/controller/EBooksController.java
@@ -0,0 +1,20 @@
+package pl.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import pl.model.User;
+
+@Controller
+public class EBooksController {
+
+	@GetMapping("/ebooks")
+	public String register(Model model)
+    {
+		new CheckAuth(model);
+        return "ebooks";
+    }
+}
